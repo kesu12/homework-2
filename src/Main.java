6464450d85ad2321b43870206207ed433e735f65
Alexey Karpovich <drflemus@gmail.com>
2025-05-07 18:05:27 +0300

    done 3 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] arr = {45, 78, -15, 12, 34, 6, 90, 7, -2, -45};
+        int max = arr[0];
+        int min = arr[0];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int num : arr) {
+            if (num > max) max = num;
+            if (num < min) min = num;
         }
+
+        System.out.println("Максимум: " + max);
+        System.out.println("Минимум: " + min);
+
+
+
+        String even = "";
+        String odd = "";
+
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                even += (" ") + num;
+            } else {
+                odd += (" ") + num;
+            }
+        }
+
+        System.out.println("Четные числа:" + even);
+        System.out.println("Нечетные числа:" + odd);
+
+        String maxNumbers = "";
+        String minNumbers = "";
+        int maxLength = Integer.MIN_VALUE;
+        int minLength = Integer.MAX_VALUE;
+
+        for (int num : arr) {
+            String strNum = Integer.toString(num);
+            int currentLength = strNum.length();
+
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+                maxNumbers = strNum;
+            } else if (currentLength == maxLength) {
+                maxNumbers += ", " + strNum;
+            }
+
+            if (currentLength < minLength) {
+                minLength = currentLength;
+                minNumbers = strNum;
+            } else if (currentLength == minLength) {
+                minNumbers += ", " + strNum;
+            }
+        }
+
+        System.out.println("Самое длинное число: " + maxNumbers);
+        System.out.println("Самое короткое число: " + minNumbers);
+
     }
 }
